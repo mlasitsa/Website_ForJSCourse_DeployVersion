6887f099efaf3d94ec7a5930ae26979dd5fcb6d9
Max Lasitsa <150858712+mlasitsa@users.noreply.github.com>
2024-01-27 14:37:42 -0600

    updates

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,17 +9,17 @@ const Header = () => {
     <div className="header">
       <div className="header-content">
         <div className="jsLogo">
-          <img src={jsLogo} alt="JS Logo" className="logo" />
+          <img src={jsLogo} alt="JS Logo"/>
         </div>
         <div className="middle" >
           <h1>JavaScript Course</h1>
         </div>
         <div className="right">
           <a href="https://www.linkedin.com/in/maxlasitsa/" target="_blank" >
-            <img src={linkedinLogo} alt="LinkedIn Logo" className="logo" />
+            <img src={linkedinLogo} alt="LinkedIn Logo"/>
           LinkedIn</a>
           <a href="https://github.com/mlasitsa" target="_blank"  >
-          <img src={githubLogo} alt="GitHub Logo" className="logo" />
+          <img src={githubLogo} alt="GitHub Logo"/>
           GitHub</a>
         </div>
       </div>
